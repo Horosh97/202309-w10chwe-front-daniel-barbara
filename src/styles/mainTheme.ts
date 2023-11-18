98edbb938c055b8cefca74f185fff3fbb8d0cb3b
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-11-18 13:52:45 +0100

    Add mainThem

---
+++ src/styles/mainTheme.ts
@@ -0,0 +1,17 @@
+import { DefaultTheme } from "styled-components";
+
+const mainTheme: DefaultTheme = {
+  borderRadius: "20px",
+  typography: {
+    fontFamily: "'Lexend Variable', sans-serif;",
+  },
+  colors: {
+    backgroundColor: "#EBFFF5",
+    fontColor: "#090909",
+    fontSecondColor: "#355A53",
+    secondBackColor: "#CFF0F2",
+    accentColor: "#1E3330",
+  },
+};
+
+export { mainTheme };
