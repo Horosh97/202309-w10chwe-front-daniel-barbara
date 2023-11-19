e60d10f6f392b5d7ffe862a5c03bbe66cb9b63a3
Daniel Andreu <daniel1997ar@gmail.com>
2023-11-19 17:47:40 +0100

    Add addRobotOnSubmit function in AddRobotPage.tsx

--- src/pages/AddRobotPage/AddRobotPage.tsx
+++ src/pages/AddRobotPage/AddRobotPage.tsx
@@ -1,8 +1,28 @@
+import { useNavigate } from "react-router-dom";
 import RobotForm from "../../components/RobotForm/RobotForm";
 import AddRobotPageStyled from "./AddRobotPageStyled";
+import { useDispatch } from "react-redux";
+import {
+  RobotStructure,
+  RobotStructureWithoutId,
+} from "../../store/features/robots/types";
+import { addRobotActionCreator } from "../../store/features/robots/robotsSlice";
+import useRobotsApi from "../../hooks/useRobotsApi";
 
 const AddRobotPage = (): React.ReactElement => {
-  const addRobotOnSubmit = () => {};
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { addNewRobot } = useRobotsApi();
+
+  const addRobotOnSubmit = async (newRobot: RobotStructureWithoutId) => {
+    try {
+      const returnedRobotFromApi: RobotStructure = await addNewRobot(newRobot);
+      dispatch(addRobotActionCreator(returnedRobotFromApi));
+      navigate("/home");
+    } catch (error) {
+      console.log((error as Error).message);
+    }
+  };
   return (
     <AddRobotPageStyled>
       <h1 className="main-heading">Create a new robot:</h1>
