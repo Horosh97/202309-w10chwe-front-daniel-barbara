77522cca0fe2652eb97ff262b76d9840f9525ef9
Barbara Gimeno <barbaragimeno.cm@gmail.com>
2023-11-19 23:26:23 +0100

    Change style of robot card

--- src/components/RobotsCard/RobotCarstyled.ts
+++ src/components/RobotsCard/RobotCarstyled.ts
@@ -1,39 +1,54 @@
 import styled from "styled-components";
 
 const RobotCardStyled = styled.article`
-  padding-right: 5px;
-  padding-left: 5px;
+  padding: 8px;
+  padding-top: 25px;
+  padding-bottom: 25px;
   display: flex;
-  gap: 16px;
-  background-color: ${({ theme }) => theme.colors.backgroundColor};
+  flex-direction: row;
+  justify-content: space-evenly;
+  gap: 14px;
+  background-color: ${({ theme }) => theme.colors.secondBackColor};
   border-radius: 25px;
   box-shadow: 0 1px 8px ${({ theme }) => theme.colors.accentColor};
   min-height: 220px;
+  height: auto;
+  width: 100%;
+  margin-top: 20px;
+  align-items: center;
+
   .card {
     &__image {
-      aspect-ratio: 1/1;
       max-width: 32cqw;
       height: auto;
-      border-radius: 50%;
+      border-radius: 15%;
+      box-shadow: 0 1px 8px #cff0f2;
+      aspect-ratio: 1/1;
       border: 2px solid ${({ theme }) => theme.colors.accentColor};
       object-fit: cover;
       box-shadow: 0 1px 8px ${({ theme }) => theme.colors.secondBackColor};
     }
 
+    &__box {
+      display: flex;
+      flex-direction: column;
+      gap: inherit;
+    }
+
     &__name {
-      font-size: 5cqw;
+      font-size: 8cqw;
       color: ${({ theme }) => theme.colors.fontColor};
       text-shadow: 0.1px 0.1px 3px ${({ theme }) => theme.colors.accentColor};
     }
 
     &__resistance {
-      font-size: 5cqw;
+      font-size: 4cqw;
       color: ${({ theme }) => theme.colors.fontColor};
       text-shadow: 0.1px 0.1px 3px ${({ theme }) => theme.colors.accentColor};
     }
 
     &__speed {
-      font-size: 5cqw;
+      font-size: 4cqw;
       color: ${({ theme }) => theme.colors.fontColor};
       text-shadow: 0.1px 0.1px 3px ${({ theme }) => theme.colors.accentColor};
     }
